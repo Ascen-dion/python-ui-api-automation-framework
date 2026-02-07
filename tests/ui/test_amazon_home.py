384e127cd4556f94c3dcc2596f9e929a39796bad
viplove <viplov.bisen@gmail.com>
2026-02-07 16:28:20 +0530

    fixessssss

--- tests/ui/test_amazon_home.py
+++ tests/ui/test_amazon_home.py
@@ -3,9 +3,7 @@ from pages.amazon_home_page import AmazonHomePage
 
 
 @allure.title("Verify Amazon homepage loads successfully")
-def test_amazon_homepage_load(driver):
-    driver.get("https://www.amazon.in")
-
+def test_amazon_homepage_load(driver,base_url):
     home = AmazonHomePage(driver)
-
+    home.open(base_url)
     assert home.ui.is_visible(home.SEARCH_BOX)
