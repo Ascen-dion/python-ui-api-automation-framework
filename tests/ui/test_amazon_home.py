4299a4484e1aa9313ea9aed93495220a2279be35
viplove <viplov.bisen@gmail.com>
2026-02-07 17:48:27 +0530

    remove files

--- tests/ui/test_amazon_home.py
+++
@@ -1,9 +0,0 @@
-import allure
-from pages.amazon_home_page import AmazonHomePage
-
-
-@allure.title("Verify Amazon homepage loads successfully")
-def test_amazon_homepage_load(driver,base_url):
-    home = AmazonHomePage(driver)
-    home.open(base_url)
-    assert home.ui.is_visible(home.SEARCH_BOX)
